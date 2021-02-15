addbdf3a1dc805a7a90b309428c60f5637e07bcb
mathews225 <petemathews14@gmail.com>
2021-02-15 11:16:23 -0500

    get perimeter

--- GeometricShapes/Program.cs
+++ GeometricShapes/Program.cs
@@ -3,7 +3,10 @@
 namespace GeometricShapes {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            var quad1 = new Quad(3, 4, 5, 6);
+            var perimeter = quad1.GetPerimeter();
+            Console.WriteLine($"quad1's perimeter is {perimeter}.");
         }
     }
 }
